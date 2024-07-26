a4556bb00ea1715ac7a957eb573a164341cca3d4
israeljbsilva <israel.bernardes.silva@gmail.com>
2024-07-26 16:50:51 -0300

    teste terraform rds postgress

--- infrastructure/provider.tf
+++ infrastructure/provider.tf
@@ -8,7 +8,7 @@ terraform {
   backend "s3" {
     bucket         = "poc-webapi-terraform-state-bucket"
     key            = "my-app/terraform.tfstate"
-    region         = "eu-north-1"
+    region         = "us-east-2"
     dynamodb_table = "terraform-locks"
   }
 }
